80a48df8d4d53a564b998cf70c1e870a21618dc2
FranciskaDendooven <franciska.dendooven@gmail.com>
2022-06-29 12:29:55 +0200

    Print required assignments of exercise two to the screen

--- exercise2/index.php
+++ exercise2/index.php
@@ -9,17 +9,21 @@ error_reporting(E_ALL);
 // TODO: Make class beer that extends from Beverage.
 // TODO: Create the properties name (string) and alcoholPercentage (float).
 // TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholPercentage.
-
 // Remember for now we will use properties and methods that can be accessed from everywhere.
 // TODO: Make a getAlcoholPercentage function which returns the alcoholPercentage.
+
+// TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
+// TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
+// TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
+// TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways(as a child?), print the color on the screen and the getInfo.
 
 class Beverage
     {
         public ?string $color;
-        public ?int $price;
+        public ?float $price;
         public $temperature;
     
-        public function __construct(?string $color, ?int $price, $temperature)
+        public function __construct(?string $color, ?float $price, $temperature)
         {
             $this->color = $color;
     
@@ -35,39 +39,39 @@ class Beer extends Beverage
     public ?string $name;
     public ?float $alcoholPercentage;
 
-    public function __construct(?string $name, ?float $alcoholPercentage)
+    public function __construct(?string $color, ?float $price, $temperature, ?string $name, ?float $alcoholPercentage)
     {
+        parent::__construct($color, $price, $temperature);
+        Beverage::__construct($color, $price, $temperature);
         $this->name = $name;
         $this->alcoholPercentage = $alcoholPercentage;
     }
 
 public function getAlcoholPercentage()
 {
-    echo "This beverage contains $this->alcoholPercentage% alcohol!";
+    echo "This beverage contains $this->alcoholPercentage% alcohol!<br>";
 }
+public function getDuvel()
+{
+        echo "This $this->name is $this->color, costs €$this->price, contains $this->alcoholPercentage% alcohol and is best served $this->temperature.<br>";
+}
+public function getDuvel2()
+{
+        echo "This $this->name is $this->color, costs €$this->price, contains $this->alcoholPercentage% alcohol and is best served $this->temperature.<br>";
+}
+public function getColor()
+{
+        echo "This is the color: $this->color.";
 }
 
-$beer1 = new Beer("", 5.5);
+}
+
+$beer1 = new Beer("blond", 0, "", "", 5.5);
+$beer2 = new Beer("blond", 3.5, "", "Duvel", 8.5);
+$beer3 = new Beer("blond", 3.5, "", "Duvel", 8.5);
+$beer4 = new Beer("blond", 0, null, "", 0);
 
 $beer1->getAlcoholPercentage();
-
-
-
-
-
-/* EXERCISE 2
-
-TODO: Make class beer that extends from Beverage.
-TODO: Create the properties name (string) and alcoholPercentage (float).
-TODO: Foresee a construct that's allows us to use all the properties from beverage and that sets the values for name and alcoholPercentage.
-
-Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getAlcoholPercentage function which returns the alcoholPercentage.
-TODO: Instantiate an object which represents Duvel. Make sure that the color is set to blond, the price equals 3.5 euro and the temperature to cold automatically.
-TODO: Also the name equal to Duvel and the alcohol percentage to 8,5%
-TODO: Print the getAlcoholPercentage 2 times on the screen in two different ways, print the color on the screen and the getInfo.
-
-Make sure that each print is on a different line.
-Try to get this error on the screen= Fatal error: Uncaught Error: Call to undefined method Beverage::getAlcoholPercentage() in /var/www/becode/workshop/exercise2.php on line 64
-USE TYPEHINTING EVERYWHERE!
-*/
+$beer2->getDuvel();
+$beer3->getDuvel2();
+$beer4->getColor();
